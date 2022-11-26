02f001c3dde5b239389ca8b293eb61ed9f7591be
Jordan <jbrockerrudow@gmail.com>
2022-11-26 09:49:27 -0600

    Add Menu Data

---
+++ restaurant/menu_data.py
@@ -0,0 +1,23 @@
+menu_item_data = [
+    {
+        "pk": 1,
+        "name": "Greek salad",
+        "price": 12,
+        "menu_item_description":  "Our famous Greek salad of crispy lettuce, peppers, olives, and our Chicago-style feta cheese. Garnished with crispy onion and salty capers."
+    }, {
+        "pk": 2,
+        "name": "Grilled fish",
+        "price": 9,
+        "menu_item_description":  "The fish is swiftly grilled over medium- or high-heat coals or over medium- or high-heat gas grill burners. Thinner fillets and steaks are grilled over direct fire."
+    }, {
+        "pk": 3,
+        "name": "Bruschetta",
+        "price": 11,
+        "menu_item_description":  "An Italian antipasto called bruschetta is made of grilled bread that has been smeared with garlic and seasoned with salt and olive oil. Toppings of tomato, veggies, beans, cured pork, or cheese are examples of variations. In Italy, a brustolina grill is frequently used to create bruschetta."
+    }, {
+        "pk": 4,
+        "name": "Lemon dessert",
+        "price": 7,
+        "menu_item_description":  "This cake is adored not only for its flavor but also for its texture and simplicity. A base of creamed butter and sugar, eggs, lemon, milk, and flour are among the most basic ingredients. We omitted the brown sugar and substituted extra granulated sugar instead."
+    }
+]
