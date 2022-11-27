415a1d5d7bc38b8d2289911a41c2a8b86eced792
Jordan <jbrockerrudow@gmail.com>
2022-11-27 09:12:54 -0600

    Update Menu Model

--- restaurant/models.py
+++ restaurant/models.py
@@ -14,7 +14,7 @@ class Booking(models.Model):
 class Menu(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField(null=False)
-    menu_item_description = models.TextField(max_length=1000, default=" ")
-
+    description = models.TextField(max_length=1000, default='')
+          
     def __str__(self):
-        return f"{self.name}, ${self.price}"
+       return self.name
