bb3f38dcd1a1c9e2e9f72955c903ff54e12efccb
Jordan <jbrockerrudow@gmail.com>
2022-11-27 15:37:04 +0000

    Formatted with Black

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -124,7 +124,6 @@ STATICFILES_DIRS = [
     "static/admin",
     "static/restaurant/css",
     "static/restaurant/img",
-
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
