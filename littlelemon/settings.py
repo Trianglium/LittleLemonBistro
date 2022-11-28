2ab8e371a78a71a13ecbef87b2c45fc73f9bc0fd
Jordan <jbrockerrudow@gmail.com>
2022-11-28 09:56:36 -0600

    Add Static config

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "restaurant/static/"
+STATIC_URL = 'restaurant/static/'
+
 STATICFILES_DIRS = [
     "restaurant/static",
-    "static/restaurant",
-    "static/admin",
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -132,4 +132,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = "/home/jordanbr/littlelemon/media"
 MEDIA_URL = "/media/"
-STATIC_ROOT = "/home/jordanbr/littlelemon/static"
+
