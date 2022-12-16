fab2e2731834e5dc17b4908964c2a32f2af7fa78
Jordan <jbrockerrudow@gmail.com>
2022-12-16 12:27:15 -0600

    Add Header and Footer to app

--- littlelemon/littlelemon/App.js
+++ littlelemon/littlelemon/App.js
@@ -1,10 +1,28 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./navigators/RootNavigator";
+import { View } from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#495E57',
+        }}>
+        <LittleLemonHeader />
+      </View>
+
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+
+      <View style={{ backgroundColor: '#495E57' }}>
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }
