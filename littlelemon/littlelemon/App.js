3bc8ad8622dad1c6cc9e9864be7c67d4b3369ba8
Jordan <jbrockerrudow@gmail.com>
2022-12-17 08:44:50 -0600

    Add Tab Navigation

--- littlelemon/littlelemon/App.js
+++ littlelemon/littlelemon/App.js
@@ -3,21 +3,41 @@ import { View, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import MenuItems from './components/MenuItems';
+import WelcomeScreen from './WelcomeScreen';
+import LoginScreen from './LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <MenuItems />
-      </View>
       <NavigationContainer>
-        <RootNavigator />
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({size}) => {
+                let iconName;
+                if (route.name === 'Welcome') {
+                  iconName = 'ios-home';
+                } else if (route.name === 'Login') {
+                  iconName = 'ios-enter';
+                }
+                return <Ionicons name={iconName} size={size} />;
+              },
+            })}
+            initialRouteName="Login">
+            <Tab.Screen name="Welcome" component={WelcomeScreen} />
+            <Tab.Screen name="Login" component={LoginScreen} />
+          </Tab.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
       </NavigationContainer>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
     </>
   );
 }
