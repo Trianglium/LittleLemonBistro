ae3359e1a44820888371131c6348efd8cf0549e8
Jordan <jbrockerrudow@gmail.com>
2022-12-19 11:16:11 -0600

    Update Button white space

--- littlelemon/littlelemon/screens/SubscribeScreen.js
+++ littlelemon/littlelemon/screens/SubscribeScreen.js
@@ -120,6 +120,7 @@ const styles = StyleSheet.create({
     borderColor: '#495E57',
     color: '#fff',
     borderRadius: '25px',
+    whiteSpace: 'normal',
   },
   buttonText: {
     color: 'white',
